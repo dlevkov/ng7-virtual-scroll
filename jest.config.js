c344e96048d2166d4aed9b19c321386ebde05dca
Denis <dlevkov@gmail.com>
2018-11-10 12:12:46 +0200

    jest setup updated

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  preset: "jest-preset-angular",
-  roots: ["src"],
-  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.ts",
-  moduleNameMapper: {
-    "@app/(.*)": "<rootDir>/src/app/$1",
-    "@assets/(.*)": "<rootDir>/src/assets/$1",
-    "@core/(.*)": "<rootDir>/src/app/core/$1",
-    "@env": "<rootDir>/src/environments/environment",
-    "@src/(.*)": "<rootDir>/src/src/$1",
-    "@state/(.*)": "<rootDir>/src/app/state/$1",
-    "@lib/(.*)": "<rootDir>/src/lib/$1"
-  }
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  "transformIgnorePatterns": [
+    "node_modules/(?!@ngrx|ngx-lorem-ipsum)"
+  ],
+  "moduleNameMapper": {
+    "^src/(.*)": "<rootDir>/src/$1",
+    "^app/(.*)": "<rootDir>/src/app/$1",
+    "^assets/(.*)": "<rootDir>/src/assets/$1",
+    "^environments/(.*)": "<rootDir>/src/environments/$1"
+  },
 };
