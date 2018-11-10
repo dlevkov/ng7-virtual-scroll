a99dd3d1da9b76a68abb7067302723fa2e7d5b0b
Denis <dlevkov@gmail.com>
2018-11-10 12:13:27 +0200

    tests adjusted

--- src/app/components/list-item/list-item.component.spec.ts
+++ src/app/components/list-item/list-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListItemComponent } from './list-item.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 
 describe('ListItemComponent', () => {
   let component: ListItemComponent;
@@ -8,7 +9,8 @@ describe('ListItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListItemComponent ]
+      declarations: [ ListItemComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,6 +18,7 @@ describe('ListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListItemComponent);
     component = fixture.componentInstance;
+    component.item = { title: 'tit', content: 'con' };
   });
 
   it('should create', () => {
